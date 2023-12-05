0f0ed46b20f02217c456988618e018160dd18996
xwilliamxiao <80023759+xwilliamxiao@users.noreply.github.com>
2023-12-04 22:34:04 -0500

    Added search ability

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -145,3 +145,164 @@ const CreateWorkout = () => {
 };
 
 export default CreateWorkout;
+
+// --- The following is with search, determine if delete later
+/*import React, { useState, useEffect } from 'react';
+import Badge from 'react-bootstrap/Badge';
+import NavBar from "../navigation/Navigation";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+const CreateWorkout = () => {
+    const [exercise_name, setExerciseName] = useState('');
+    const [exercise_description, setExerciseDescription] = useState('');
+    const [muscle_group, setMuscleGroup] = useState('');
+    const [workoutCards, setWorkoutCards] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // Gets the existing data from our database
+    useEffect(() => {
+        fetch('http://localhost:8000/create/')
+            .then(response => response.json())
+            .then(data => setWorkoutCards(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []); // Empty dependency array ensures the effect runs once on mount
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        // Check if all fields are filled
+        if (!exercise_name || !exercise_description || !muscle_group) {
+            alert('Complete all required fields to create exercise.');
+            return;
+        }
+
+        // Create a new user-created exercise
+        const newUserExercise = {
+            exercise_name,
+            exercise_description,
+            muscle_group,
+        };
+
+        // Update the state with the new card
+        setWorkoutCards([...workoutCards, newUserExercise]);
+
+        // Clear the input fields
+        setExerciseName('');
+        setExerciseDescription('');
+        setMuscleGroup('');
+
+        // Send a POST request to save the user-created exercise to the backend
+        fetch('http://localhost:8000/create/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newUserExercise),
+        })
+            .then(response => response.json())
+            .then(data => console.log('User-created exercise saved:', data))
+            .catch(error => console.error('Error saving user-created exercise:', error));
+    };
+
+    // Filter exercises based on search term
+    const filteredExercises = workoutCards.filter(exercise =>
+        exercise.exercise_name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const tooltip = <Tooltip>Add exercises for the community.</Tooltip>;
+
+    return (
+        <div style={{ backgroundColor: '#F3F3F3' }}>
+            <NavBar/>
+            <main className='Container'>
+                <div className="row justify-content-center gap-4" style={{ padding: '40px' }}>
+                    {/!* Create Exercise Card *!/}
+                    <div className="card mb-4 col-lg-3 col-md-8 col-sm-8 col-8 align-items-center border border-3 ">
+                        <div className="card-body">
+                            <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '20px' }}>Add Exercises</h3>
+                            <p>Complete the following criteria below to add exercises into the community.</p>
+                            <form onSubmit={handleFormSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="exercise_name" className="form-label">Exercise Name:</label>
+                                    <input
+                                        type="text"
+                                        id="exercise_name"
+                                        className="form-control"
+                                        value={exercise_name}
+                                        onChange={(e) => setExerciseName(e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exercise_description" className="form-label">Exercise Description:</label>
+                                    <input
+                                        type="text"
+                                        id="exercise_description"
+                                        className="form-control"
+                                        value={exercise_description}
+                                        onChange={(e) => setExerciseDescription(e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="muscle_group" className="form-label">Muscle Group:</label>
+                                    <select
+                                        id="muscle_group"
+                                        className="form-control"
+                                        value={muscle_group}
+                                        onChange={(e) => setMuscleGroup(e.target.value)}
+                                    >
+                                        <option value="Biceps">Biceps</option>
+                                        {/!* ... (other muscle groups) ... *!/}
+                                    </select>
+                                </div>
+                                <OverlayTrigger placement="bottom" overlay={tooltip}>
+                                    <button type="submit" className="btn btn-primary" style={{ marginTop: '20px', marginBottom: '20px'}}>Generate</button>
+                                </OverlayTrigger>
+                            </form>
+                        </div>
+                    </div>
+
+                    {/!*--- Display generated workout cards with search functionality ---*!/}
+                    <div className="card mb-4 col-lg-8 col-md-8 col-sm-8 col-8 border border-3" >
+                        <div className="card-body">
+                            <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '20px' }}>Community Exercises</h3>
+                            <p>Below is a list of exercises that were created by our community.</p>
+                            {/!* Search bar *!/}
+                            <div className="mb-3">
+                                {/!*<label htmlFor="searchTerm" className="form-label">Search Exercises:</label>*!/}
+                                <input
+                                    type="text"
+                                    id="searchTerm"
+                                    className="form-control"
+                                    placeholder="Search exercises"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        {/!*--- Exercise Cards ---*!/}
+                        <div className="row row-cols-1 row-cols-md-1 row-cols-lg-3 g-4" style={{ padding: '20px' }}>
+                            {filteredExercises.length > 0 ? (
+                                filteredExercises.map((card) => (
+                                    <div key={card.id} className="col mb-4">
+                                        <div className="card h-100 shadow-sm border border-1">
+                                            <div className="card-body">
+                                                <h5 className="card-title mb-3">{card.exercise_name}</h5>
+                                                <Badge variant="info" className="muscle-group-badge bg-dark">{card.muscle_group}</Badge>
+                                                <p className="card-text mt-3">{card.exercise_description}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>No matching exercises found.</p>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default CreateWorkout;*/
--- frontend/src/pages/Exercise.js
+++ frontend/src/pages/Exercise.js
@@ -1,126 +1,10 @@
-/*
-import React, { useState } from 'react';
-import NavBar from "../navigation/Navigation";
-
-const Exercise = () => {
-    // Create a list first to test if the UI will print out the correct style
-    // Replace this stuff with my database after
-    const exercises = [
-        {
-            id: 1,
-            name: 'Exercise 1',
-            description: 'Description for Exercise 1',
-            muscleGroup: 'Muscle Group 1',
-        },
-        {
-            id: 2,
-            name: 'Exercise 2',
-            description: 'Description for Exercise 2',
-            muscleGroup: 'Muscle Group 2',
-        },
-        {
-            id: 3,
-            name: 'Exercise 3',
-            description: 'Description for Exercise3',
-            muscleGroup: 'Muscle Group 3',
-        },
-        {
-            id: 4,
-            name: 'Exercise 4',
-            description: 'Description for Exercise4',
-            muscleGroup: 'Muscle Group 4',
-        },
-    ];
-
-    return (
-        <div>
-            <NavBar/>
-        <main className='Container'>
-            <div className="row justify-content-center gap-4 " style={{ padding: '40px'}}>
-                <div className="card mb-4 col-lg-8 col-md-8 col-sm-8 col-8">
-                    <div>
-                        <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '50px', marginLeft:'40px'}}>Exercise List</h3>
-                    </div>
-                    <div className="d-flex flex-wrap gap-3 align-items-center justify-content-center" style={{ padding: '20px' }}>
-                        {exercises.map((exercise) => (
-                            <div key={exercise.id} className="col-lg-3 col-md-8">
-                                <div className="card mb-4">
-                                    <div className="card-body">
-                                        <h3 className="card-title mb-3">{exercise.name}</h3>
-                                        <badge className="badge rounded-pill bg-dark">{exercise.muscleGroup}</badge>
-                                        <p className="card-text mb-4">
-                                            {exercise.description}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </main>
-        </div>
-    );
-};
-
-export default Exercise;*/
-
-/*
-import React, { useState, useEffect } from 'react';
-import NavBar from "../navigation/Navigation";
-
-const Exercise = () => {
-    const [exercises, setExercises] = useState([]);
-
-    useEffect(() => {
-        // Use the fetch API or a library like axios to make a request to your Django backend
-        // Replace the URL with the actual endpoint of your Django API
-        fetch('http://localhost:8000/exercise_library/')
-            .then(response => response.json())
-            .then(data => setExercises(data))
-            .catch(error => console.error('Error fetching exercises:', error));
-    }, []); // The empty dependency array means this effect runs once after the initial render
-
-    return (
-        <div>
-            <NavBar/>
-            <main className='Container'>
-                <div className="row justify-content-center gap-4 " style={{ padding: '40px'}}>
-                    <div className="card mb-4 col-lg-8 col-md-8 col-sm-8 col-8">
-                        <div>
-                            <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '50px', marginLeft:'40px'}}>Exercise List</h3>
-                        </div>
-                        <div className="d-flex flex-wrap gap-3 align-items-center justify-content-center" style={{ padding: '20px' }}>
-                            {exercises.map((exercise) => (
-                                <div key={exercise.id} className="col-lg-3 col-md-8">
-                                    <div className="card mb-4">
-                                        <div className="card-body">
-                                            <h3 className="card-title mb-3">{exercise.name}</h3>
-                                            <badge className="badge rounded-pill bg-dark">{exercise.muscle_group}</badge>
-                                            {/!*<span className="badge rounded-pill bg-dark">{exercise.muscle_group}</span>*!/}
-                                            <p className="card-text mb-4">
-                                                {exercise.description}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </div>
-    );
-};
-
-export default Exercise;*/
-
 import React, { useState, useEffect } from 'react';
 import NavBar from "../navigation/Navigation";
 
 const Exercise = () => {
     const [exercises, setExercises] = useState([]);
     const [selectedMuscleGroup, setSelectedMuscleGroup] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const apiUrl = selectedMuscleGroup
@@ -133,51 +17,14 @@ const Exercise = () => {
             .catch(error => console.error('Error fetching exercises:', error));
     }, [selectedMuscleGroup]);
 
-    const muscleGroups = ['All Exercises','Biceps', 'Triceps', 'Shoulders', 'Chest', 'Back', 'Quadriceps', 'Hamstring', 'Calf', 'Abs'];
+    const muscleGroups = ['All Exercises', 'Biceps', 'Triceps', 'Shoulders', 'Chest', 'Back', 'Quadriceps', 'Hamstring', 'Calf', 'Abs'];
+
+    // Filter exercises based on search term
+    const filteredExercises = exercises.filter(exercise =>
+        exercise.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
-        /*<div>
-            <NavBar />
-            <main className='Container'>
-                <div className="row justify-content-center gap-4 " style={{ padding: '40px' }}>
-                    <div className="card col-lg-3 col-md-8 col-sm-8 col-8 align-items-center">
-                        <div className="list-group w-100">
-                            {muscleGroups.map((group, index) => (
-                                <button
-                                    key={index}
-                                    type="button"
-                                    className={`list-group-item list-group-item-action ${group === selectedMuscleGroup || (group === 'All Exercises' && selectedMuscleGroup === null) ? 'active' : ''}`}
-                                    onClick={() => setSelectedMuscleGroup(group === 'All Exercises' ? null : group)}
-                                >
-                                    {group}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="card mb-4 col-lg-8 col-md-8 col-sm-8 col-8">
-                        <div>
-                            <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '50px', marginLeft: '40px' }}>Exercise List</h3>
-                        </div>
-                        <div className="row row-cols-1 row-cols-md-3 g-4" style={{ padding: '20px' }}>
-                            {exercises.map((exercise) => (
-                                <div key={exercise.id} className="col-md-4">
-                                    <div className="card h-100 mb-4">
-                                        <div className="card-body">
-                                            <h3 className="card-title mb-3">{exercise.name}</h3>
-                                            <span className="badge rounded-pill bg-dark">{exercise.muscle_group}</span>
-                                            <p className="card-text mb-4">
-                                                {exercise.description}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </div>*/
-
         <div style={{ backgroundColor: '#F3F3F3' }}>
             <NavBar />
             <main className='Container'>
@@ -199,6 +46,92 @@ const Exercise = () => {
                     </div>
 
                     {/* Exercise Cards */}
+                    <div className="card mb-4 col-lg-8 col-md-8 col-sm-8 col-8 border border-3">
+                        <div className="card-body">
+                            <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '20px', marginBottom: '20px'}}>Exercise List</h3>
+                            <p>View list of exercises.</p>
+                            <input
+                                type="text"
+                                className="form-control mb-3"
+                                placeholder="Search exercises"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                            {/*<br/><hr/>*/}
+
+                        </div>
+                        <div className="row row-cols-1 row-cols-md-3 g-4" style={{ padding: '20px' }}>
+                            {filteredExercises.length > 0 ? (
+                                filteredExercises.map((exercise) => (
+                                    <div key={exercise.id} className="col-md-4">
+                                        <div className="card h-100 shadow-sm mb-4">
+                                            <div className="card-body">
+                                                <h5 className="card-title mb-3">{exercise.name}</h5>
+                                                <span className="badge rounded-pill bg-dark">{exercise.muscle_group}</span>
+                                                <p className="card-text mt-3">
+                                                    {exercise.description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>No exercises found</p>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default Exercise;
+
+// --- The following code is what it looks like without the search functionality ---
+/*
+import React, { useState, useEffect } from 'react';
+import NavBar from "../navigation/Navigation";
+
+const Exercise = () => {
+    const [exercises, setExercises] = useState([]);
+    const [selectedMuscleGroup, setSelectedMuscleGroup] = useState(null);
+
+    useEffect(() => {
+        const apiUrl = selectedMuscleGroup
+            ? `http://localhost:8000/exercise_library/?muscle_group=${selectedMuscleGroup}`
+            : 'http://localhost:8000/exercise_library/';
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => setExercises(data))
+            .catch(error => console.error('Error fetching exercises:', error));
+    }, [selectedMuscleGroup]);
+
+    const muscleGroups = ['All Exercises','Biceps', 'Triceps', 'Shoulders', 'Chest', 'Back', 'Quadriceps', 'Hamstring', 'Calf', 'Abs'];
+
+    return (
+        <div style={{ backgroundColor: '#F3F3F3' }}>
+            <NavBar />
+            <main className='Container'>
+                <div className="row justify-content-center gap-4 " style={{ padding: '40px' }}>
+                    {/!* List Group *!/}
+                    <div className="col-lg-3 col-md-8 col-sm-8 col-8 align-items-center">
+                        <div className="list-group w-100 border border-3">
+                            {muscleGroups.map((group, index) => (
+                                <button
+                                    key={index}
+                                    type="button"
+                                    className={`list-group-item list-group-item-action ${group === selectedMuscleGroup || (group === 'All Exercises' && selectedMuscleGroup === null) ? 'active' : ''}`}
+                                    onClick={() => setSelectedMuscleGroup(group === 'All Exercises' ? null : group)}
+                                >
+                                    {group}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+
+                    {/!* Exercise Cards *!/}
                     <div className="card mb-4 col-lg-8 col-md-8 col-sm-8 col-8 border border-3">
                         <div className="card-body">
                             <h3 className="card-title mb-3 fw-bold" style={{ marginTop: '20px', marginBottom: '20px'}}>Exercise List</h3>
@@ -227,4 +160,4 @@ const Exercise = () => {
     );
 };
 
-export default Exercise;
+export default Exercise;*/
